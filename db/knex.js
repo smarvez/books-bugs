ea964830adb9b9722503a09bd0fe10f6492aeac9
Stephanie Marvez <marvez@gmail.com>
2018-01-22 09:51:50 -0700

    bugs fixed

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,7 @@
+'use strict';
+
 var environment = process.env.NODE_ENV || 'development' ;
 var config = require('../knexfile')[environment];
-module.exports = require('knex')(config);
+var knex = require('knex')(config);
+
+module.exports = knex;
